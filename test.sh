3cf150a7c7a1fc735d29f45b62d7e31b0dc0d220
Xiaojun Chen <xiaojunch@gmail.com>
2017-06-01 22:37:01 -0400

    test

---
+++ test.sh
@@ -0,0 +1,16 @@
+
+function git_color {
+  local git_status="$(git status 2> /dev/null)"
+
+  if [[ ! $git_status =~ "working directory clean" ]]; then
+    echo -e $COLOR_RED
+  elif [[ $git_status =~ "Your branch is ahead of" ]]; then
+    echo -e $COLOR_YELLOW
+  elif [[ $git_status =~ "nothing to commit" ]]; then
+    echo -e $COLOR_GREEN
+  else
+    echo -e $COLOR_OCHRE
+  fi
+}
+
+echo $(git_color)
